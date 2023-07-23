b978d3c78f3dd261aa702ab6c9380589b3f5a8df
Josue Morales <molinajosue92@hotmail.com>
2023-07-23 12:31:44 -0400

    Initial Commit

---
+++ VoyageAtlas/VoyageAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyageAtlasApp.swift
+//  VoyageAtlas
+//
+//  Created by Josue morales on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct VoyageAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
